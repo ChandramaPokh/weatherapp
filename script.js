9435500b36305b68b53cb867533dd0c6058f2bb8
ChandramaPokh <pokheralchandrama@gmail.com>
2026-06-03 14:59:21 +0545

    final touches

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ const humidity_data = document.querySelector(".humidity_data");
 const wind_data = document.querySelector(".wind_data");
 const pressure_data = document.querySelector(".pressure_data");
 const container = document.querySelector(".container");
-//   cityName.textContent= "Kathmandu, Nepal"
+const weathertemperature = document.querySelector(".weathertemperature");
+const citysearch = document.querySelector(".citysearch");
+ 
+citysearch.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    city = Searched.value;
+    getWeatherdata();
+   
+})
+
 
 function getCountry(p){
 
@@ -34,25 +43,29 @@ function getDateandTime(dt){
 
 }
 
-
+let city =  "kathmandu"
 const getWeatherdata = async ()=>{
-     let city =  "kathmandu"
 
     const url  = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=21e5c164bd740e911e96b5054f1fa9de&units=metric`;
     
     try {
-
         
         const response = await fetch(url);
         const  data =  await response.json();
         console.log(data);
-       const {main,sys,weather, wind, name,dt}= data;
-       cityName.innerHTML = `${name}, ${getCountry(sys.country)} `;
-          dateandtime.innerHTML = getDateandTime(dt);
-          weatherforecast.innerHTML =weather[0].main;
-          weathericon.innerHTML = `<img src ="https://openweathermap.org/img/wn/${weather[0].icon}@2x.png">`
-      
-
+        const {main,sys,weather, wind, name,dt}= data;
+        cityName.innerHTML = `${name}, ${getCountry(sys.country)} `;
+         dateandtime.innerHTML = getDateandTime(dt);
+         weatherforecast.innerHTML =weather[0].main;
+         weathericon.innerHTML = `<img src ="https://openweathermap.org/img/wn/${weather[0].icon}@2x.png">`
+         weathertemperature.textContent = `${Math.round(main.temp)}°C`;
+         maxtemp.textContent = `Max : ${main.temp_max.toFixed()}°C`;
+         mintemp.textContent =`Min : ${main.temp_min.toFixed()}°C`;
+         feelslike_data.textContent = `${main.feels_like.toFixed()}°C`;
+         humidity_data.textContent =`${main.humidity}°C`;
+         wind_data.textContent =`${wind.speed}m/s`;
+         pressure_data.textContent =`${main.pressure}hPa`;
+          Searched.value="";
 
 
     } catch (error) {
@@ -62,3 +75,4 @@ const getWeatherdata = async ()=>{
 
 }
 document.body.addEventListener('load',getWeatherdata())
+
